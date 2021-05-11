b4b26f1bb41797d3c33951c3742d8c1c644371bf
Soniaghoshal <43308572+Soniaghoshal@users.noreply.github.com>
2021-05-11 12:14:57 +0530

    Create Models-register.cs

---
+++ Models-register.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication6.Models
+{
+    public class register
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
+        public Int64 phone { get; set; }
+    }
+}
